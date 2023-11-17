a47d2268a095ee33d48933e586268948c3a69190
Jose Cabrera <cabreraj525@gmail.com>
2023-11-17 15:53:45 -0600

    agregamos midleware para manejo de errores

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes'
 import { envToConst } from './shared/envToConst'
+import { errorHandler } from './shared/middleware/error'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,6 +16,9 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use(routes)
+
+//midleware para manejo de errores a nivel de toda la app
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`)
