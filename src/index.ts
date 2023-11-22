1fa2e582a6774969e35ded01e3eeb14aa38b86ca
Jose Cabrera <cabreraj525@gmail.com>
2023-11-22 00:36:09 -0600

    agregamos morgan

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
 import routes from './routes'
 import { envToConst } from './shared/envToConst'
 import { errorHandler } from './shared/middleware/error'
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(morgan('dev'))
 
 const corsOptions = {
   origin: envToConst.CORS_ALLOWED,
