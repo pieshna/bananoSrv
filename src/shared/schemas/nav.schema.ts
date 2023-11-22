2650e3c0ab9b90c02b44f7087bac5573877e2456
Jose Cabrera <cabreraj525@gmail.com>
2023-11-22 00:36:31 -0600

    corregimos schema de navbar

--- src/shared/schemas/nav.schema.ts
+++ src/shared/schemas/nav.schema.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
 
 export const navSchema = z.object({
-  id: z.string().uuid().optional(),
-  title: z.string().nonempty(),
-  link: z.string().nonempty(),
+  navbar_id: z.string().uuid().optional(),
+  title: z.string().min(1),
+  link: z.string().min(1),
   icon: z.string().optional(),
   parent_id: z.string().uuid().optional(),
   created_at: z.date().optional(),
