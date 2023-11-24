0efc35afd92af9be88212e6809589e5db8d384ca
Jose Cabrera <cabreraj525@gmail.com>
2023-11-23 19:22:12 -0600

    eliminamos funciones inecesarias

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,6 @@ import express from 'express'
 // rutas esenciales en shared folder
 import authRoutes from './shared/endpoints/auth/auth.routes'
 import navRoutes from './shared/endpoints/nav/nav.routes'
-import connection from './shared/db/connection'
 import { authMiddleware } from './shared/middleware/auth'
 // Endpoints del proyecto
 import permisoRoutes from './permisos/permiso.routes'
@@ -13,8 +12,6 @@ const router = express.Router()
 
 // Endpoint para probar el servidor
 router.get('/', async (req, res) => {
-  const conexion = await connection.query('SELECT 1 + 1 AS solution')
-  console.log(conexion)
   res.json({ message: 'Servidor funcionando correctamente' })
 })
 
