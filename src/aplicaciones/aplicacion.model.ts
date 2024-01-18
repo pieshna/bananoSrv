902ba1d98783d2aa623fdf4b1a0c5a33bb49a061
Pieshna <cabreraj525@gmail.com>
2024-01-17 22:25:07 -0600

    Add create method to AplicacionModel

--- src/aplicaciones/aplicacion.model.ts
+++ src/aplicaciones/aplicacion.model.ts
@@ -4,6 +4,15 @@ class AplicacionModel extends ModelWithUUID {
   constructor() {
     super('aplicacion', 'aplicacion_id')
   }
+
+  async create(json: unknown): Promise<any> {
+    const added = await super.create(json)
+    await super.findByQuery(
+      'insert into precios (precio_quintal_compra,aplicacion_id) values (0,uuid_to_bin(?))',
+      added.insertId
+    )
+    return added
+  }
 }
 
 export default new AplicacionModel()
