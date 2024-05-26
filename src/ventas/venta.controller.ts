c1974e1b44317ff982cfc341084de161268e8763
Pieshna <cabreraj525@gmail.com>
2024-05-26 09:38:35 -0600

    Agregar controlador y rutas para obtener datos de ventas por empresa y fechas

--- src/ventas/venta.controller.ts
+++ src/ventas/venta.controller.ts
@@ -14,6 +14,30 @@ export const getVenta = asyncHandler(async (req: Request, res: Response) => {
   handleDataAndResponse(res, result)
 })
 
+export const getDataVentasEmpresa = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { empresa_id } = req.params
+    const result = await ventaModel.getDataVentas(empresa_id)
+    handleDataAndResponse(res, result)
+  }
+)
+
+export const getDataVentasEmpresaByDates = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { empresa_id } = req.params
+    const { fecha_inicio, fecha_fin } = req.query
+    if (!fecha_inicio || !fecha_fin) {
+      throw new Error('Fechas de inicio y fin son requeridas')
+    }
+    const result = await ventaModel.getDataVentasByDates(
+      fecha_inicio as string,
+      fecha_fin as string,
+      empresa_id
+    )
+    handleDataAndResponse(res, result)
+  }
+)
+
 export const createVenta = asyncHandler(async (req: Request, res: Response) => {
   const result = await ventaModel.create(req.body)
   handleDataAndResponse(res, result, 201)
